6c1ab8cb0c7ee01b1b65c16daba3c0a904cb42b8
Oyewale <oyewalefavour424@gmail.com>
2023-10-27 14:18:22 +0100

    initializing getshoe bug fixes

--- lib/pages/produxts/product_page.dart
+++ lib/pages/produxts/product_page.dart
@@ -7,6 +7,8 @@ import 'package:provider/provider.dart';
 
 import '../../model/sneakers_model.dart';
 
+// import '../../model/sneakers_model.dart';
+
 class ProductPage extends StatefulWidget {
   final String id;
   final String category;
@@ -18,147 +20,150 @@ class ProductPage extends StatefulWidget {
 
 class _ProductPageState extends State<ProductPage> {
   final PageController pageController = PageController();
-  late Future<Sneakers> _sneakers;
+  late Future<Sneakers> _sneaker;
 
   void getShoes() {
     if (widget.category == "Men's Running") {
-      _sneakers = Helper().getMaleSneakersById(widget.id);
+      _sneaker = Helper().getMaleSneakersById(widget.id);
     } else if (widget.category == "Women's Running") {
-      _sneakers = Helper().getFemaleSneakersById(widget.id);
+      _sneaker = Helper().getFemaleSneakersById(widget.id);
     } else {
-      _sneakers = Helper().getKidsSneakersById(widget.id);
+      _sneaker = Helper().getKidsSneakersById(widget.id);
     }
+  }
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        body: FutureBuilder<Sneakers>(
-          future: _sneakers,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator(
-                color: Colors.black,
-              );
-            } else if (snapshot.hasError) {
-              return Text("Error: ${snapshot.error}");
-            } else {
-              final sneakers = snapshot.data;
-              return Consumer<ProductNotifier>(
-                builder: (context, productNotifier, child) {
-                  return CustomScrollView(
-                    slivers: [
-                      SliverAppBar(
-                        automaticallyImplyLeading: false,
-                        leadingWidth: 0,
-                        title: Padding(
-                          padding: const EdgeInsets.only(bottom: 10),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              GestureDetector(
-                                onTap: null,
-                                child: const Icon(Icons.close),
-                              ),
-                              GestureDetector(
-                                onTap: null,
-                                child: const Icon(Ionicons.ellipse),
-                              )
-                            ],
-                          ),
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getShoes();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<Sneakers>(
+        future: _sneaker,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator(
+              color: Colors.black,
+            );
+          } else if (snapshot.hasError) {
+            return Text("Error: ${snapshot.error}");
+          } else {
+            final sneaker = snapshot.data;
+            return Consumer<ProductNotifier>(
+              builder: (context, productNotifier, child) {
+                return CustomScrollView(
+                  slivers: [
+                    SliverAppBar(
+                      automaticallyImplyLeading: false,
+                      leadingWidth: 0,
+                      title: Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            GestureDetector(
+                              onTap: null,
+                              child: const Icon(Icons.close),
+                            ),
+                            GestureDetector(
+                              onTap: null,
+                              child: const Icon(Ionicons.ellipse),
+                            )
+                          ],
                         ),
-                        pinned: true,
-                        snap: false,
-                        floating: true,
-                        backgroundColor: Colors.transparent,
-                        expandedHeight: MediaQuery.of(context).size.height,
-                        flexibleSpace: FlexibleSpaceBar(
-                          background: Stack(
-                            children: [
-                              SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.5,
-                                width: MediaQuery.of(context).size.height,
-                                child: PageView.builder(
-                                  scrollDirection: Axis.horizontal,
-                                  itemCount: sneakers!.imageUrl.length,
-                                  controller: pageController,
-                                  onPageChanged: (page) {
-                                    productNotifier.activePage = page;
-                                  },
-                                  itemBuilder: (context, int index) {
-                                    return Stack(
-                                      children: [
-                                        Container(
-                                          height: MediaQuery.of(context)
-                                                  .size
-                                                  .height *
-                                              0.39,
-                                          width:
-                                              MediaQuery.of(context).size.width,
-                                          color: Colors.grey.shade300,
-                                          child: CachedNetworkImage(
-                                            imageUrl: sneakers.imageUrl[index],
-                                            fit: BoxFit.contain,
-                                          ),
+                      ),
+                      pinned: true,
+                      snap: false,
+                      floating: true,
+                      backgroundColor: Colors.transparent,
+                      expandedHeight: MediaQuery.of(context).size.height,
+                      flexibleSpace: FlexibleSpaceBar(
+                        background: Stack(
+                          children: [
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.5,
+                              width: MediaQuery.of(context).size.height,
+                              child: PageView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemCount: sneaker!.imageUrl.length,
+                                controller: pageController,
+                                onPageChanged: (page) {
+                                  productNotifier.activePage = page;
+                                },
+                                itemBuilder: (context, int index) {
+                                  return Stack(
+                                    children: [
+                                      Container(
+                                        height:
+                                            MediaQuery.of(context).size.height *
+                                                0.39,
+                                        width:
+                                            MediaQuery.of(context).size.width,
+                                        color: Colors.grey.shade300,
+                                        child: CachedNetworkImage(
+                                          imageUrl: sneaker.imageUrl[index],
+                                          fit: BoxFit.contain,
                                         ),
-                                        Positioned(
-                                          top: MediaQuery.of(context)
-                                                  .size
-                                                  .height *
-                                              0.09,
-                                          right: 20,
-                                          child: const Icon(
-                                            Ionicons.heart,
-                                            color: Colors.grey,
-                                          ),
+                                      ),
+                                      Positioned(
+                                        top:
+                                            MediaQuery.of(context).size.height *
+                                                0.09,
+                                        right: 20,
+                                        child: const Icon(
+                                          Ionicons.heart,
+                                          color: Colors.grey,
                                         ),
-                                        Positioned(
-                                          bottom: 0,
-                                          right: 0,
-                                          left: 0,
-                                          height: MediaQuery.of(context)
-                                                  .size
-                                                  .height *
-                                              0.3,
-                                          child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.center,
-                                            children: List<Widget>.generate(
-                                              sneakers.imageUrl.length,
-                                              (index) => Padding(
-                                                padding: const EdgeInsets.symmetric(
-                                                  horizontal: 4,
-                                                ),
-                                                child: CircleAvatar(
-                                                  radius: 5,
-                                                  backgroundColor:
-                                                      productNotifier
-                                                                  .activePage !=
-                                                              index
-                                                          ? Colors.grey
-                                                          : Colors.black,
-                                                ),
+                                      ),
+                                      Positioned(
+                                        bottom: 0,
+                                        right: 0,
+                                        left: 0,
+                                        height:
+                                            MediaQuery.of(context).size.height *
+                                                0.3,
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: List<Widget>.generate(
+                                            sneaker.imageUrl.length,
+                                            (index) => Padding(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                horizontal: 4,
+                                              ),
+                                              child: CircleAvatar(
+                                                radius: 5,
+                                                backgroundColor: productNotifier
+                                                            .activePage !=
+                                                        index
+                                                    ? Colors.grey
+                                                    : Colors.black,
                                               ),
                                             ),
                                           ),
-                                        )
-                                      ],
-                                    );
-                                  },
-                                ),
-                              )
-                            ],
-                          ),
+                                        ),
+                                      )
+                                    ],
+                                  );
+                                },
+                              ),
+                            )
+                          ],
                         ),
-                      )
-                    ],
-                  );
-                },
-              );
-            }
-          },
-        ),
-      );
-    }
+                      ),
+                    )
+                  ],
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
   }
 }
